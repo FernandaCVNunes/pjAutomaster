b327af16e0b7c07fa6e24855d5e6bd77db1c61d6
Josdegar <josdegarfsantos@gmail.com>
2025-08-24 16:48:31 -0300

    conexões com o banco configuradas

--- server/models/database.js
+++ server/models/database.js
@@ -1,16 +1,25 @@
 const mysql = require('mysql2/promise');
 
-// Configurações de conexão com o seu banco de dados
 const pool = mysql.createPool({
-    host: '127.0.0.1',       // Endereço do seu servidor MySQL
-    user: 'root',     // Seu usuário do MySQL Workbench
-    password: 'root0208',     // Sua senha
-    database: 'dbmecanica', // O nome do banco que você criou
+    host: '127.0.0.1',       
+    user: 'root',     
+    password: '1234',  
+    database: 'bdmecanica',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-console.log("Conexão com o banco de dados estabelecida com sucesso!");
+async function testarConexao() {
+    try {
+        const connection = await pool.getConnection();
+        console.log("Conexão com o banco de dados estabelecida com sucesso!");
+        connection.release(); // libera a conexão de volta para o pool
+    } catch (err) {
+        console.error("Erro ao conectar ao banco:", err.message);
+    }
+}
+
+testarConexao();
 
 module.exports = pool;
